91520c690bff781b7fefe3c6a493f4a28f6bc7db
Soroosh Morshedi <93315815+Sorooshmorshedi@users.noreply.github.com>
2022-02-28 10:37:56 +0330

    fix awnser

--- wathermelon/awnser.py
+++ wathermelon/awnser.py
@@ -1,12 +1,9 @@
 number = int(input('inter the wathermelon weight : '))
-role = ''
-for i in range(1,number+1):
-    role += str(i)
-for i in range(0, len(role)-1):
-    if int(role[i]) % 2 == 0 : 
-        if (number -  int(role[i])) % 2 ==0:
+role = [i for i in range(1, number+1)]
+for i in role:
+    if i % 2 == 0 : 
+        if (number -  i) % 2 == 0:
             print('Yes')
             break
-
 else:
     print('no')
